3325877c7907e6e0f20596d6798672d26b61fbf6
Jakub Srnka <jakub.srnka@volny.cz>
2024-03-29 18:18:51 +0100

    🚸 web app capable

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,8 @@
   import '../app.pcss';
 </script>
 
+<svelte:head>
+  <meta name="apple-mobile-web-app-title" content="Jakhub Games" />
+  <meta name="apple-mobile-web-app-capable" content="yes" />
+</svelte:head>
 <slot />
