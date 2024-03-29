fa34e29bb123a56f265cd90465bf37f0968cc240
Jakub Srnka <jakub.srnka@volny.cz>
2024-03-29 19:06:29 +0100

    💄 homepage: initial commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import Game from '$components/elements/Game.svelte';
+  import { Grid3x3 } from 'lucide-svelte';
+</script>
+
+<main class="p-6">
+  <h1 class="text-center text-xl">Welcome to Jakhub Games!</h1>
+
+  <div class="m-auto mt-4 grid w-full max-w-96 grid-cols-2 gap-4">
+    <Game href="/sudoku" class="bg-violet-300">
+      <h2 slot="title">Sudoku</h2>
+      <Grid3x3 slot="icon" size={32} absoluteStrokeWidth />
+    </Game>
+  </div>
+</main>
