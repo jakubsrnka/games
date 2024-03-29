954e46836327a51347c3c83729e2b305944e4a63
Jakub Srnka <jakub.srnka@volny.cz>
2024-03-29 19:06:29 +0100

    💄 homepage: initial commit

---
+++ src/components/elements/Game.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { cn } from '$lib/utils';
+  export let href: string;
+
+  let className: string | undefined = undefined;
+  export { className as class };
+</script>
+
+<a class={cn('flex flex-col items-center gap-2 rounded-2xl pb-4 pt-4', className)} {href}>
+  <slot name="icon" />
+  <h2 class="text-lg">
+    <slot name="title" />
+  </h2>
+</a>
