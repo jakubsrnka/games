87743df5156c88215a75000e0275ca768130844b
Jakub Srnka <jakub.srnka@volny.cz>
2024-03-29 18:00:44 +0100

    ✨ sudoku: user candidates

--- src/components/sudoku/Candidates.svelte
+++ src/components/sudoku/Candidates.svelte
@@ -1,15 +1,13 @@
 <script lang="ts">
-  import type { Digit } from 'sudoku-master';
+  import { VALID_DIGITS, type Digit } from 'sudoku-master';
 
-  export let candidates: Array<Digit>;
-
-  const numberToDigit = (number: number): Digit => number as Digit;
+  export let candidates: Set<Digit>;
 </script>
 
 <div class="absolute grid h-9 w-9 grid-cols-3 text-neutral-500">
   {#each Array(9) as _, i}
     <div class="flex h-3 w-3 items-center justify-center text-[10px]">
-      {#if candidates.includes(numberToDigit(i + 1))}
+      {#if candidates.has(VALID_DIGITS[i])}
         {i + 1}
       {/if}
     </div>
