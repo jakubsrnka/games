e2d478b142c0bff69035b6667a481f4921dd5c7a
Jakub Srnka <jakub.srnka@volny.cz>
2024-12-31 02:55:44 +0100

    💄 button: loading state

--- src/components/ui/button/button.svelte
+++ src/components/ui/button/button.svelte
@@ -2,6 +2,7 @@
   import { Button as ButtonPrimitive } from 'bits-ui';
   import { cn } from '$lib/utils.js';
   import { buttonVariants, type Props, type Events } from './index.js';
+  import { Loader, Loader2 } from 'lucide-svelte';
 
   type $$Props = Props;
   type $$Events = Events;
@@ -11,15 +12,23 @@
   export let size: $$Props['size'] = 'default';
   export let builders: $$Props['builders'] = [];
   export { className as class };
+  export let loading: $$Props['loading'] = false;
+  export let disabled: $$Props['disabled'] = loading;
 </script>
 
 <ButtonPrimitive.Root
   {builders}
-  class={cn(buttonVariants({ variant, size, className }))}
+  class={cn(buttonVariants({ variant, size, className }), 'relative')}
   type="button"
   {...$$restProps}
   on:click
   on:keydown
+  {disabled}
 >
   <slot />
+  {#if loading}
+    <div class="animate-spin-slow absolute right-2 grid h-6 w-6 place-content-center">
+      <Loader />
+    </div>
+  {/if}
 </ButtonPrimitive.Root>
