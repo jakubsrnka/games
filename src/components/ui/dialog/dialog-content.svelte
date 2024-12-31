897b6dc7c7c356871ad087ca80a045b45c804810
Jakub Srnka <jakub.srnka@volny.cz>
2024-12-31 02:56:35 +0100

    💄 shadcn: dialog: initial commit

---
+++ src/components/ui/dialog/dialog-content.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import { Dialog as DialogPrimitive } from 'bits-ui';
+  import Cross2 from 'svelte-radix/Cross2.svelte';
+  import * as Dialog from './index.js';
+  import { cn, flyAndScale } from '$lib/utils.js';
+  import DisableAutofocus from '$components/elements/DisableAutofocus.svelte';
+
+  type $$Props = DialogPrimitive.ContentProps & {
+    disableClose?: boolean;
+  };
+
+  let className: $$Props['class'] = undefined;
+  export let transition: $$Props['transition'] = flyAndScale;
+  export let transitionConfig: $$Props['transitionConfig'] = {
+    duration: 200
+  };
+  export { className as class };
+  export let disableClose = false;
+</script>
+
+<Dialog.Portal>
+  <Dialog.Overlay />
+  <DialogPrimitive.Content
+    {transition}
+    {transitionConfig}
+    class={cn(
+      'fixed left-[50%] top-[50%] z-50 grid w-full max-w-[90vw] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-xl border bg-background p-6 shadow-2xl md:w-full md:max-w-lg',
+      className
+    )}
+    {...$$restProps}
+  >
+    <DisableAutofocus />
+    <slot />
+    {#if !disableClose}
+      <DialogPrimitive.Close
+        class="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground"
+      >
+        <Cross2 class="h-4 w-4" />
+        <span class="sr-only">Close</span>
+      </DialogPrimitive.Close>
+    {/if}
+  </DialogPrimitive.Content>
+</Dialog.Portal>
