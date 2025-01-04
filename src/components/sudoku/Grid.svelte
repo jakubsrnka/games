a75054c3cd18a4f876e95c3dd526d69f5edd606d
Jakub Srnka <jakub.srnka@volny.cz>
2025-01-04 01:47:00 +0100

    ✨ sudoku: save sudoku to Supabase

--- src/components/sudoku/Grid.svelte
+++ src/components/sudoku/Grid.svelte
@@ -6,23 +6,31 @@
     type GridIndex,
     type Digit,
     parseGrid,
-    VALID_GRID_INDEXES
+    VALID_GRID_INDEXES,
+    type SudokuGrid
   } from 'sudoku-master';
-  import { getDigit } from '$lib/sudoku';
+  import {
+    decodeGrid,
+    getDigit,
+    getKeyValuePairsFromCandidatesString,
+    getKeyValuePairsFromGridString,
+    getMapFromGrid,
+    stringifyCandidates,
+    stringifyGrid
+  } from '$lib/sudoku';
   import { getSudoku } from 'sudoku-gen';
   import Script from '$components/utils/Script.svelte';
   import { candidatesToObject, figureOutCandidates } from '$lib/sudoku/candidates';
   import Candidates from './Candidates.svelte';
+  import { onMount } from 'svelte';
+  import { user } from '$lib/client/user';
+  import { supabase } from '$lib/shared/supabase';
 
-  const grid = parseGrid(getSudoku($sudokuSettings.difficulty).puzzle, true);
+  let grid: SudokuGrid | null = null;
 
-  if (grid === null) {
-    throw new Error('Failed to parse grid');
-  }
+  let solution: ReadonlyMap<GridIndex, Digit> | null = null;
 
-  const solution = solveWithBacktracking(grid) as ReadonlyMap<GridIndex, Digit> | null;
-
-  let candidates = candidatesToObject(grid.candidates);
+  let candidates: { [key: number]: Set<Digit> } = {};
 
   let selected: GridIndex | null;
 
@@ -30,7 +38,8 @@
 
   let userCandidates: { [key: number]: Set<Digit> } = {};
 
-  const setDigit = (digit: Digit) => {
+  export const setDigit = (digit: Digit) => {
+    if (!grid) return;
     if (selected !== null) {
       userSolution[selected] = digit;
       if ($sudokuSettings.autoCandidates) {
@@ -40,15 +49,20 @@
         selected = null;
       }
     }
+
+    updateChanges();
   };
 
-  const removeDigit = () => {
+  export const removeDigit = () => {
+    if (!grid) return;
     if (selected !== null) delete userSolution[selected];
     userSolution = userSolution;
     candidates = figureOutCandidates(grid.candidates, userSolution);
+
+    updateChanges();
   };
 
-  const setCandidate = (digit: Digit) => {
+  export const setCandidate = (digit: Digit) => {
     if (selected !== null) {
       if (!userCandidates[selected]) {
         userCandidates[selected] = new Set();
@@ -63,59 +77,129 @@
       }
     }
     userCandidates = userCandidates;
+
+    updateChanges();
   };
 
-  const removeCandidates = () => {
+  export const removeCandidates = () => {
     if (selected !== null && userCandidates[selected]) {
       delete userCandidates[selected];
     }
     userCandidates = userCandidates;
+
+    updateChanges();
   };
+
+  const updateChanges = async () => {
+    if (!grid) return;
+    if ($user) {
+      await supabase
+        .from('sudoku')
+        .update({
+          user_solution: stringifyGrid(getMapFromGrid(userSolution)),
+          user_candidates: stringifyCandidates(userCandidates)
+        })
+        .eq('sudoku', stringifyGrid(grid.digits));
+    }
+  };
+
+  onMount(async () => {
+    if ($user) {
+      try {
+        const { data, error } = await supabase
+          .from('sudoku')
+          .select()
+          .eq('completed', false)
+          .order('created_at', { ascending: false });
+        if (!error && data && data.length) {
+          const result = data[0];
+          grid = decodeGrid(result.sudoku, result.candidates);
+          candidates = getKeyValuePairsFromCandidatesString(result.candidates);
+          userCandidates = getKeyValuePairsFromCandidatesString(result.user_candidates);
+          userSolution = getKeyValuePairsFromGridString(result.user_solution);
+          console.log(candidates, result.candidates);
+          console.log(grid);
+          console.log(userCandidates, result.user_candidates);
+          console.log(userSolution, result.user_solution);
+          return;
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    grid = parseGrid(getSudoku($sudokuSettings.difficulty).puzzle, true);
+
+    if (!grid) {
+      return;
+    }
+
+    solution = solveWithBacktracking(grid) as ReadonlyMap<GridIndex, Digit> | null;
+
+    candidates = candidatesToObject(grid.candidates);
+    console.log(candidates, grid.candidates);
+
+    if ($user) {
+      await supabase.from('sudoku').insert([
+        {
+          sudoku: stringifyGrid(grid.digits),
+          solution: stringifyGrid(solution ?? new Map()),
+          user_solution: stringifyGrid(getMapFromGrid(userSolution)),
+          difficulty: $sudokuSettings.difficulty,
+          candidates: stringifyCandidates(candidates),
+          user_candidates: stringifyCandidates(userCandidates)
+        }
+      ]);
+    }
+  });
 </script>
 
 <div class="relative grid w-fit grid-cols-9 border border-neutral-500">
-  <LayoverGrid />
-  {#each Array.from({ length: 81 }) as _, i}
-    {@const isCorrect =
-      $sudokuSettings.showCorrect && solution && getDigit(i, solution) === userSolution[i]}
-    {#if isCorrect}
-      <Script
-        f={() => {
-          selected = null;
-        }}
-      />
-    {/if}
-    <button
-      class="relative flex h-9 w-9 items-center justify-center border-[0.5px] border-neutral-400"
-      class:border-neutral-500={selected === i}
-      class:outline={selected === i}
-      class:border-[1px]={selected === i}
-      class:outline-[0.5px]={selected === i}
-      class:outline-neutral-500={selected === i}
-      class:bg-amber-300={selected === i}
-      class:z-[100]={selected === i}
-      class:text-green-600={isCorrect}
-      class:bg-neutral-200={grid.digits.has(VALID_GRID_INDEXES[i])}
-      class:cursor-default={grid.digits.has(VALID_GRID_INDEXES[i]) || isCorrect}
-      on:click={() => {
-        if (isCorrect) {
-          selected = null;
-          return;
-        }
-        selected = getDigit(i, grid.digits) !== '' ? null : VALID_GRID_INDEXES[i];
-      }}
-    >
-      <span class="text-xl">
-        {getDigit(i, grid.digits)}
-        {userSolution[i] ?? ''}
-      </span>
-      {#if !userSolution[i] && $sudokuSettings.autoCandidates && candidates[i]}
-        <Candidates candidates={candidates[i]} />
-      {:else if userCandidates[i]}
-        <Candidates candidates={userCandidates[i]} />
+  {#if grid}
+    <LayoverGrid />
+    {#each Array.from({ length: 81 }) as _, i}
+      {@const isCorrect =
+        $sudokuSettings.showCorrect && solution && getDigit(i, solution) === userSolution[i]}
+      {#if isCorrect}
+        <Script
+          f={() => {
+            selected = null;
+          }}
+        />
       {/if}
-    </button>
-  {/each}
+      <button
+        class="relative flex h-9 w-9 items-center justify-center border-[0.5px] border-neutral-400"
+        class:border-neutral-500={selected === i}
+        class:outline={selected === i}
+        class:border-[1px]={selected === i}
+        class:outline-[0.5px]={selected === i}
+        class:outline-neutral-500={selected === i}
+        class:bg-amber-300={selected === i}
+        class:z-[100]={selected === i}
+        class:text-green-600={isCorrect}
+        class:bg-neutral-200={grid.digits.has(VALID_GRID_INDEXES[i])}
+        class:cursor-default={grid.digits.has(VALID_GRID_INDEXES[i]) || isCorrect}
+        on:click={() => {
+          if (!grid) return;
+          if (isCorrect) {
+            selected = null;
+            return;
+          }
+          selected = getDigit(i, grid.digits) !== '' ? null : VALID_GRID_INDEXES[i];
+        }}
+      >
+        <span class="text-xl">
+          {getDigit(i, grid.digits)}
+          {userSolution[i] ?? ''}
+        </span>
+        {#if !userSolution[i] && $sudokuSettings.autoCandidates && candidates[i]}
+          <Candidates candidates={candidates[i]} />
+        {:else if userCandidates[i]}
+          <Candidates candidates={userCandidates[i]} />
+        {/if}
+      </button>
+    {/each}
+  {/if}
 </div>
 
 <slot {setDigit} {removeDigit} {setCandidate} {removeCandidates} />
--- src/lib/sudoku/index.ts
+++ src/lib/sudoku/index.ts
@@ -1,6 +1,93 @@
-import type { Digit, GridIndex } from 'sudoku-master';
+import { type Digit, type GridIndex, type SudokuGrid } from 'sudoku-master';
 
 export const difficulties = ['easy', 'medium', 'hard', 'expert'] as const;
 
-export const getDigit = (index: number, grid: ReadonlyMap<GridIndex, Digit>) =>
-  grid.get(index as GridIndex) ?? '';
+/**
+ * Get the digit at the given index in the grid. If the cell is empty, return the replace value.
+ * @param index Index of the cell, from 0 to 80
+ * @param grid The grid to get the digit from
+ * @param replace Missing value to return if the cell is empty
+ * @returns The digit at the given index, or the replace value if the cell is empty
+ */
+export const getDigit = (index: number, grid: ReadonlyMap<GridIndex, Digit>, replaceMissing = '') =>
+  grid.get(index as GridIndex) ?? replaceMissing;
+
+export const getMapFromGrid = (grid: { [key: number]: Digit }): ReadonlyMap<GridIndex, Digit> => {
+  const map = new Map<GridIndex, Digit>();
+  for (const [key, value] of Object.entries(grid)) {
+    map.set(parseInt(key) as GridIndex, value);
+  }
+  return map;
+};
+
+/**
+ * Export the grid as a string where empty cells are represented by a dot. This string is always 81 characters long.
+ * @param grid
+ * @returns A string representation of the grid
+ */
+export const stringifyGrid = (grid: ReadonlyMap<GridIndex, Digit>) =>
+  Array.from({ length: 81 }, (_, index) => getDigit(index, grid, '.')).join('');
+
+export const getMapFromGridString = (grid: string): ReadonlyMap<GridIndex, Digit> => {
+  const map = new Map<GridIndex, Digit>();
+  for (let i = 0; i < 81; i++) {
+    const digit = grid.charAt(i);
+    if (digit !== '.') {
+      map.set(i as GridIndex, digit as unknown as Digit);
+    }
+  }
+  return map;
+};
+
+export const getKeyValuePairsFromGridString = (grid: string): { [key: number]: Digit } => {
+  const pairs: { [key: number]: Digit } = {};
+  for (let i = 0; i < 81; i++) {
+    const digit = grid.charAt(i);
+    if (digit !== '.') {
+      pairs[i] = digit as unknown as Digit;
+    }
+  }
+  return pairs;
+};
+
+export const stringifyCandidates = (candidates: { [key: number]: Set<Digit> }) =>
+  Array.from({ length: 81 }, (_, index) => {
+    const set = candidates[index];
+    return set
+      ? Array.from({ length: 9 }, (_, digit) =>
+          set.has((digit + 1) as Digit) ? (digit + 1).toString() : '.'
+        ).join('')
+      : '.........';
+  }).join('');
+
+export const getKeyValuePairsFromCandidatesString = (
+  candidates: string
+): { [key: number]: Set<Digit> } => {
+  const pairs: { [key: number]: Set<Digit> } = {};
+  for (let i = 0; i < 81; i++) {
+    const set = new Set<Digit>();
+    const start = i * 9;
+    for (let j = 0; j < 9; j++) {
+      const digit = candidates.charAt(start + j);
+      if (digit !== '.') {
+        set.add(parseInt(digit) as unknown as Digit);
+      }
+    }
+    if (set.size > 0) {
+      pairs[i] = set;
+    }
+  }
+  return pairs;
+};
+
+export const decodeGrid = (grid: string, candidates: string): SudokuGrid => {
+  const digits = getMapFromGridString(grid);
+  const candidatesMap = getKeyValuePairsFromCandidatesString(candidates);
+  const candidatesMapWithArrays = new Map<GridIndex, readonly Digit[]>(
+    Array.from(Object.entries(candidatesMap), ([key, value]) => [
+      parseInt(key) as GridIndex,
+      Array.from(value)
+    ])
+  );
+  return { digits, candidates: candidatesMapWithArrays };
+};
